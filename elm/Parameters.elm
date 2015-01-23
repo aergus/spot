b6e99a89e7ad76ea506ec3349813943e887fcb1d
Aras Ergus <alpha.ergus@gmail.com>
2015-01-23 10:18:32 +0100

    Scalable initial field

--- elm/Parameters.elm
+++ elm/Parameters.elm
@@ -3,6 +3,7 @@ module Parameters where
 import Color
 import List
 
+import Auxiliary (..)
 import Types (..)
 
 dimension : Int
@@ -24,10 +25,10 @@ blockBg : Color.Color
 blockBg = Color.rgb 200 200 200
 
 initField : Int -> GameField
-initField n = [[[], [], [], [(2, 1)]],
-               [[], [], [], []],
-               [[], [], [], []],
-               [[], [(2, 1)], [], []]]
+initField n = indexedMatrixMap (\ p x -> if p == (n // 2, 0) || p == (n // 2, n // 3)
+                                         then [p]
+                                         else x)
+                               (emptyField n)
 
 emptyField : Int -> GameField
 emptyField n = List.repeat n (List.repeat n [])
