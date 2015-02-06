ec10fe90db7e522c8c1af540f6a2110dbbcefd8f
Aras Ergus <alpha.ergus@gmail.com>
2015-02-06 13:54:38 +0100

    Show numbers in blocks (+ new coloring)

--- elm/Drawing.elm
+++ elm/Drawing.elm
@@ -5,25 +5,38 @@ import Dict
 import Graphics.Element
 import Graphics.Collage
 import List
+import Text
 
 import Auxiliary (..)
 import Parameters (..)
 import Types (..)
 
-colorOf : Block -> Color.Color
-colorOf l = if List.isEmpty l
+colorOf : Int -> Color.Color
+colorOf l = if l == 0
             then blockBg
-            else let n = List.length l - 1
-                 in Color.rgb ((211 * n) % 255) ((223 * n) % 255) 50
+            else let n = l - 1
+                 in Color.rgb ((221 * n) % 255) ((25 * n) % 255) 250
 
 
 toForms : GameField -> List Graphics.Collage.Form
 toForms f = let toOffset k = -sceneSize / 2 + blockSize / 2 + marginSize +
                              (blockSize + marginSize) * toFloat k in
   Dict.values (Dict.map
-    (\ (i, j) x -> Graphics.Collage.move (toOffset j, -(toOffset i))
-                                         (Graphics.Collage.filled (colorOf x)
-                                                                  (Graphics.Collage.rect blockSize blockSize))
+    (\ (i, j) x ->
+      let l = List.length x
+          c = colorOf l
+          form = Graphics.Collage.filled c (Graphics.Collage.rect blockSize blockSize)
+      in Graphics.Collage.move (toOffset j, -(toOffset i))
+                               (if l == 0
+                                then form
+                                else Graphics.Collage.group
+        [form,
+         (Graphics.Collage.toForm << Text.centered
+                                  << Text.height (blockSize * 0.5)
+                                  << Text.color Color.black
+                                  << Text.bold
+                                  << Text.fromString
+                                  << toString) (2 * l)])
     )
     f)
 
